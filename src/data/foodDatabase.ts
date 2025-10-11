05d129266fe888d249f7d099a20c5af136954570
chandrababuk-gif <chandrababu.k@gmail.com>
2025-10-11 13:59:11 +0530

    Updated foodDatabase.ts

--- src/data/foodDatabase.ts
+++ src/data/foodDatabase.ts
@@ -1,3 +1,5 @@
+import foodItems from './Food items.json';
+
 export interface FoodItem {
   Food: string;
   ItemCalories: number;
@@ -5,14 +7,7 @@ export interface FoodItem {
   Micronutrients: string;
 }
 
-export const foodDatabase: FoodItem[] = [
-  {
-    Food: "Chapati (whole wheat)",
-    ItemCalories: 297,
-    Nutrients: "9.0Protein(g), 48.0Carbs(g), 7.0Fat(g), 6.5Fiber(g), 45Calcium(mg), 3.5Iron(mg), 76Magnesium(mg), 180Potassium(mg), 420Sodium(mg), 1.8Zinc(mg)",
-    Micronutrients: "0Vit A(μg RAE), 0Vit C(mg), 0.18Vit B6(mg), 38Folate(μg), 0Vit B12(μg)"
-  }
-];
+export const foodDatabase: FoodItem[] = foodItems as FoodItem[];
 
 export function parseNutrients(nutrients: string) {
   const matches = nutrients.match(/(\d+\.?\d*)Protein\(g\), (\d+\.?\d*)Carbs\(g\), (\d+\.?\d*)Fat\(g\)/);
