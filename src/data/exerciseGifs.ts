806ee5e4b0f0a1987981be18ecc0a6fb155405b1
sanketvagam <sanketvagam@gmail.com>
2025-10-12 01:31:57 +0530

    Updated exerciseGifs.ts

--- src/data/exerciseGifs.ts
+++ src/data/exerciseGifs.ts
@@ -1,6 +1,6 @@
 export const exerciseGifMap: Record<string, string> = {
   'jumping jacks': 'https://cdn.dribbble.com/userupload/23995967/file/original-b7327e47be94975940e98b26277e5ead.gif',
-  'push-ups': 'https://media.tenor.com/L7tYMDkJGlMAAAAM/push-up.gif',
+  'push-ups': 'https://tenor.com/tcupsnoJBUB.gif',
   'squats': 'https://media.tenor.com/YwGJNH9kVGAAAAAM/squat.gif',
   'plank': 'https://media.tenor.com/xB6zMVR5yYsAAAAM/plank.gif',
   'burpees': 'https://media.tenor.com/W5KvXxjGGUMAAAAM/burpee.gif',
